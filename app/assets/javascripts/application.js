99408db95f870ca2abd06e1c366aa4eb069fa29d
themladjo <mladenjanic@gmail.com>
2017-06-02 21:29:46 +0200

    Add TinyMCE

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+    tinymce.remove();
+    tinymce.init({
+        selector:'textarea#post_body',
+        plugins: "codesample image link media textcolor",
+        toolbar: "undo redo | fontselect fontsizeselect styleselect | bold italic | codesample image link | media forecolor backcolor",
+        font_formats: 'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats',
+        fontsize_formats: '8pt 10pt 12pt 14pt 18pt 24pt 36pt'
+    });
+})
